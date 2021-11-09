9042d3ba370dc03b01c81b3c363ea3e92fd3ee44
harbouli <mharbouli@icloud.com>
2021-11-09 13:28:16 +0100

    Puah

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  
+  return(
+    <>
+    <Navbar></Navbar>
+      <Component {...pageProps} />
+    </>
+    )
 }
 
 export default MyApp
