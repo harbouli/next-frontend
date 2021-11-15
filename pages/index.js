d136951e249f9819a5419aee7f667d13fc70b322
harbouli <mharbouli@icloud.com>
2021-11-15 11:03:22 +0100

    0.6.12

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,38 @@
 import Services from '../components/AllServices'
 import Hero from '../components/Hero'
 import About from '../components/About'
+import Head from 'next/head'
+import { fetchAPI } from '../lib/api'
 
 
 
-
-export default function Home({services}) {
+export default function Home({home}) {
 
   return (
-    <div>
+    <>
+      <Head>
+      <meta charset="UTF-8" />
+      <title>{home.Seo.Title}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="keywords" content={home.Seo.keywords}/>
+        <meta name="description" content={home.Seo.Description} />
+        <meta property="og:title" content={home.WebSiteTitet} />
+        <meta property="og:url" content="https://www.exampel.com/" />
+        <meta property="og:image" content={home.Seo.open_graph_image.url} />
+      </Head>
       <Hero/>
       <About/>
       <Services service={services} />
-    </div>
+    </>
   )
 }
 
 
  export async function getStaticProps () {
-   const {API_URL} = process.env
-   const res = await fetch(`${API_URL}/services`)
-   const data = await res.json()
+   const homeAPI =  fetchAPI('home')
    return {
        props: { 
-          services: data
+          home: homeAPI
        }, 
        revalidate: 3600,
      }
